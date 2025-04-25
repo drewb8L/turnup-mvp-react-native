26390db04bdc343d5f58e15bcd0cf1c44cfca1db
Andrew Babcock <ababcock@8thlight.com>
2025-04-24 17:02:14 -0700

    trigger GH actions

---
+++ app/components/test-comp.tsx
@@ -0,0 +1,13 @@
+export default const TestComp = () => {
+    return (
+        <View
+        style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+        }}
+        >
+        <Text>Just a test.</Text>
+        </View>
+    );
+}
