520c0439b55a8ab8c1da96c5739fd4f6ad9a94af
Andrew Babcock <ababcock@8thlight.com>
2025-04-24 17:02:50 -0700

    trigger GH actions

--- app/components/__tests__/index.test.tsx
+++ app/components/__tests__/index.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react-native';
-import Index from "../../index";
+import Index from "@/index";
 
-test('basic test', () => {
-    render(<Index />);
-    expect(screen.getByText(/edit/i)).toBeOnTheScreen()
+test('basic test', async () => {
+    render(<Index/>);
+    const element = await screen.findByText(/edit/i, {}, { timeout: 5000 });
+    expect(element).toBeTruthy();
 
 });
